59ed570c8d6edb68c29d76d133581cb66047d5d2
YOUR USERNAMErobpwatkins <robpwatkins@gmail.com>
2020-04-06 09:34:30 -0500

    oh we did it.

--- src/FetchTest.js
+++ src/FetchTest.js
@@ -2,27 +2,27 @@ import React from 'react';
 
 class FetchTest extends React.Component {
   state = {
-    pups: []
+    things: []
   }
 
   componentDidMount() {
-    fetch('https://gist.githubusercontent.com/robpwatkins/3fddbada918e592e3901a474b0953df9/raw/da884d19b47bbcc25115578c95106ffd5b8d9af5/test.json')
+    fetch('https://gist.githubusercontent.com/robpwatkins/3fddbada918e592e3901a474b0953df9/raw/ebe834bcd812e7849fa15fdf7573f010dbf93fc5/test.json')
     .then(response => response.json())
     .then(json => {
-      this.setState({ pups: [...json] })
-      console.log(this.state.pups)
+      this.setState({ things: [...json] })
+      console.log(this.state.things)
     })
   }
 
   render() {
     return(
       <div>
-        {this.state.pups.map((pup, i) => {
+        {this.state.things.map((thing, i) => {
           return (
             <div key={i}>
-              <h1>{pup.Name}</h1>
-              <h3>{pup.Breed}</h3>
-              <h3>{pup.Hometown}</h3>
+              <h1>{thing.thing}</h1>
+              <h3>{thing.description}</h3>
+              <img src={thing.imgUrl} alt=""></img>
             </div>
           )
         })}
